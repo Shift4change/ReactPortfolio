69e9c170e22d5657c12608e4ab6f081e860cbfde
Paul Fodrovics <pefodrovics@gmail.com>
2020-04-17 20:31:21 -0400

    Updates

---
+++ precache-manifest.5d2264f366d09b79b5385672cda20d68.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a990adb41f32b5b711040b9d2923f62",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "45e1085d32de19e5cdaa",
+    "url": "/ReactPortfolio/static/css/2.929bb6f5.chunk.css"
+  },
+  {
+    "revision": "2e527d7ad772b5321116",
+    "url": "/ReactPortfolio/static/css/main.eb4fef93.chunk.css"
+  },
+  {
+    "revision": "45e1085d32de19e5cdaa",
+    "url": "/ReactPortfolio/static/js/2.c0d7373c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.c0d7373c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e527d7ad772b5321116",
+    "url": "/ReactPortfolio/static/js/main.47ed2864.chunk.js"
+  },
+  {
+    "revision": "d6debfcf9016d21ba6c0",
+    "url": "/ReactPortfolio/static/js/runtime-main.9dce2d6e.js"
+  },
+  {
+    "revision": "053647217464853f1a39f71765867c76",
+    "url": "/ReactPortfolio/static/media/collab.05364721.jpg"
+  },
+  {
+    "revision": "9bd2aa5bf80db62d830b9d8092df23e2",
+    "url": "/ReactPortfolio/static/media/foodfinder.9bd2aa5b.png"
+  },
+  {
+    "revision": "0dc5d3cc29dd0dc36744df92fe76f3f0",
+    "url": "/ReactPortfolio/static/media/resume.0dc5d3cc.pdf"
+  },
+  {
+    "revision": "b7f78fe73fa25e40dcc717a304fc4d92",
+    "url": "/ReactPortfolio/static/media/solve1.b7f78fe7.png"
+  },
+  {
+    "revision": "66659ef4518025321a4bb649c3ac5471",
+    "url": "/ReactPortfolio/static/media/workout.66659ef4.png"
+  }
+]);
